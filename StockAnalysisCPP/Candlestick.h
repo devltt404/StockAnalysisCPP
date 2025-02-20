622c89898a55a96309dc349ad0164d7c8492a881
Tri Pham <devltt404@gmail.com>
2025-02-20 13:17:17 -0500

    Sort

--- StockAnalysisCPP/Candlestick.h
+++ StockAnalysisCPP/Candlestick.h
@@ -3,19 +3,19 @@ using namespace System;
 
 public ref class Candlestick
 {
-public:
-	// Property to store the date of the candlestick
-	property DateTime date;			
-	// Property to store the opening price of the stock on the day
-	property double open;
-	// Property to store the closing price of the stock on the day
-	property double close;
-	// Property to store the highest price of the stock on the day
-	property double high;				
-	// Property to store the lowest price of the stock on the day
-	property double low;				
+public:	
 	// Property to store the number of shares traded on the day
 	property unsigned long long volume;
+	// Property to store the lowest price of the stock on the day
+	property double low;
+	// Property to store the highest price of the stock on the day
+	property double high;
+	// Property to store the closing price of the stock on the day
+	property double close;
+	// Property to store the opening price of the stock on the day
+	property double open;
+	// Property to store the date of the candlestick
+	property DateTime date;
 
 	/// <summary>
 	/// Constructor to create a Candlestick object from a row of stock data values
